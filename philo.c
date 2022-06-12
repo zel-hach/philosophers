9e7d2117097ded7b109789f08847670333017fa8
Zineb  El hachimi <zel-hach@e3r3p6.1337.ma>
2022-06-12 18:55:29 +0100

    test

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: zel-hach <zel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 10:49:49 by zel-hach          #+#    #+#             */
-/*   Updated: 2022/06/12 17:09:25 by zel-hach         ###   ########.fr       */
+/*   Updated: 2022/06/12 18:46:28 by zel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,12 +45,14 @@ void	*routine(void *t)
 
 	p = (t_info_philo *)t;
 
+printf("%ld ------- %ld ---------%d\n ",get_time(),p->last_meal,p->t->time_to_die);
 	if (p->id % 2 == 0)
 		usleep (10000);
 	while (1)
 	{
-		// if(get_time() - p->last_meal < p->t->time_to_die)
-		// {
+	
+		if(get_time() - p->last_meal >=  p->t->time_to_die)
+		{
 			pthread_mutex_lock(&p->t->fork[p->id]);
 			printf("philo de id : %d took fork 1\n",p->id);
 			pthread_mutex_lock(&p->t->fork[(p->id + 1) % 4]);
@@ -67,8 +69,11 @@ void	*routine(void *t)
 			usleep(60000);
 			printf("philo de id : %d is thinking\n", p->id);
 		}
-	// 	printf("philo is die");
-	// }
+		// else
+		// {
+		// 	printf("philo is die \n");
+		// }
+	}
 }
 
 void	create(int tab[0], t_philo *t, t_info_philo *p)
@@ -76,6 +81,7 @@ void	create(int tab[0], t_philo *t, t_info_philo *p)
 	int	i;
 
 	i = 0;
+	p->last_meal = get_time();
 	while (i < tab[0])
 	{
 		p[i].id = i;
@@ -111,9 +117,13 @@ void	ft_philosophers(char **arg)
 			i++;
 		}
 		t = malloc(sizeof(t_philo) * tab[0]);
+		t->number_of_philosophers = tab[0];
+		t->time_to_die = tab[1];
+		t->time_to_eat	= tab[2];
+		t->time_to_sleep = tab[3];
+		t->nmbr_eat	= tab[4];
 		p = malloc(sizeof(t_info_philo) * tab[0]);
-		get_arg(tab,t);
-		t->fork = malloc(sizeof(pthread_mutex_t) * tab[0]);
+		t->fork = malloc(sizeof(pthread_mutex_t) * t->number_of_philosophers);
 		i = 0;
 		while (i < tab[0])
 			pthread_mutex_init(&t->fork[i++], NULL);
@@ -129,13 +139,14 @@ int	main(int argc, char **argv)
 	char	**arg;
 	
 	arg = ft_join_args(argv);
-	if(argc <= 6)
-	{
+	argc = 0;
+	// if(argc <= 6)
+	// {
 		
 		if(check_space(argv) == 0)
 			write(1, "Error\n", 6);
 		else
 			ft_philosophers(arg);
-	}
+	// }
 	return(0);
 }
